68618a35fbbb0a32e82e3dd34c90eda2d497641e
Ceprnic <ceprnic.josip@gmail.com>
2024-03-27 11:04:07 +0100

    basic project structure implemented with some seed check

---
+++ API/Program.cs
@@ -0,0 +1,29 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(); // u launchSettingsima stavi launchBrowser: false da se svakin pokretanjem ne pokrene swagger
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
---
+++ Core/Entities/Product.cs
@@ -0,0 +1,8 @@
+namespace Core.Entitites
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
