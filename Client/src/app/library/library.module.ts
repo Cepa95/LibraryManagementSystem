dd46af578477ab8faedb033195e124106e9afadb
Ceprnic <ceprnic.josip@gmail.com>
2024-04-08 11:00:04 +0200

    added book-item component& and implemented card system

--- Client/src/app/library/library.module.ts
+++ Client/src/app/library/library.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { LibraryComponent } from './library.component';
+import { BookItemComponent } from './book-item/book-item.component';
 
 
 
 @NgModule({
   declarations: [
-    LibraryComponent
+    LibraryComponent,
+    BookItemComponent
   ],
   imports: [
     CommonModule,
