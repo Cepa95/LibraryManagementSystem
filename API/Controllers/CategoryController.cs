88345e16d798c38cfbdfd8c006b8154fbd3f09ec
Ceprnic <ceprnic.josip@gmail.com>
2024-04-09 10:27:15 +0200

    made dtos for publisher and category

--- API/Controllers/CategoryController.cs
+++ API/Controllers/CategoryController.cs
@@ -1,3 +1,5 @@
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
@@ -9,19 +11,32 @@ namespace API.Controllers
     public class CategoryController : BaseApiController
     {
         private readonly IGenericRepository<Category> _categoryRepository;
-        public CategoryController(IGenericRepository<Category> categoryRepository)
+
+        private readonly ILogger<CategoryController> _logger;
+
+        private readonly IMapper _mapper;
+
+        public CategoryController(IGenericRepository<Category> categoryRepository, 
+                                  IMapper mapper,
+                                  ILogger<CategoryController> logger)
         {
             _categoryRepository = categoryRepository;
+            _mapper = mapper;
+            _logger = logger;
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Category>>> GetCategories()
+        public async Task<ActionResult<IReadOnlyList<CategoryDto>>> GetCategories()
         {
+            _logger.LogInformation("Getting all categories");
+
             var spec = new CategorySpecification();
 
             var categories = await _categoryRepository.ListAsync(spec);
 
-            return Ok(categories);
+            var categoryDtos = _mapper.Map<IReadOnlyList<CategoryDto>>(categories); 
+
+            return Ok(categoryDtos);
         }
 
 
---
+++ API/Dtos/CategoryDto.cs
@@ -0,0 +1,8 @@
+namespace API.Dtos
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
