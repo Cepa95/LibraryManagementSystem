344ba9abbbef8751f093583e1be939fd124b1e12
Ceprnic <ceprnic.josip@gmail.com>
2024-04-12 08:42:08 +0200

    implement carousel

--- Client/src/app/home/home.module.ts
+++ Client/src/app/home/home.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, CarouselModule.forRoot()],
   exports: [HomeComponent],
 })
 export class HomeModule {}
