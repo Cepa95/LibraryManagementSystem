af34d8b43f87f9e038bdaea5f6de9d90296e7d7d
Ana <arakoc00@fesb.hr>
2024-05-09 10:12:47 +0200

    loanComponent setup

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LibraryComponent } from './library/library.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
+import { LoanComponent } from './loanComponent/loan/loan.component';
 
 
 
@@ -11,7 +12,7 @@ const routes: Routes = [
 
   { path: 'library', loadChildren: () => import('./library/library.module').then(m => m.LibraryModule) },
   { path: 'account', loadChildren: () => import('./loginAndRegister/login-and-register.module').then(m => m.LoginAndRegisterModule) },
-  // { path: '', component: LibraryComponent },
+  { path: 'loan', component: LoanComponent },
   { path: 'not-found', component: NotFoundComponent },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
---
+++ Client/src/app/loanComponent/loan/loan.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loan',
+  standalone: true,
+  imports: [],
+  templateUrl: './loan.component.html',
+  styleUrl: './loan.component.scss'
+})
+export class LoanComponent {
+
+}
