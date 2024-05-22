e7eeb4f3a2173e147882b205bbf6e065c0a25b26
ivanloncar <ivanloncar02@gmail.com>
2024-05-22 12:55:04 +0200

    Fix redundancy

--- Client/src/app/library/book-details.component.ts
+++ Client/src/app/library/book-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/models/book';
 import { LibraryService } from './library.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-book-details',
@@ -11,23 +11,13 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class BookDetailsComponent implements OnInit {
   book?: Book;
 
-  constructor(private libraryService: LibraryService, private activatedRoute: ActivatedRoute, private router: Router) { }
+  constructor(private libraryService: LibraryService, private activatedRoute: ActivatedRoute) { }
 
 
   ngOnInit(): void {
     this.loadBook();
   }
 
-  navigateToBookDetailsPage() {
-    alert('Alooo');
-    if (this.book && this.book.id) {
-      console.log(`Navigating to book details page for book id ${this.book.id}`);
-      this.router.navigate(['library', this.book.id]); // Removed the trailing slash after 'library'
-    } else {
-      console.log('Book id is not available');
-    }
-  }
-  
   loadBook() {
     const id = this.activatedRoute.snapshot.paramMap.get('id')
     if (id) this.libraryService.getBook(+id).subscribe({
